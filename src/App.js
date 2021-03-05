ac0a97f4abbffd28084b6321a555795b63adda09
Bruno Gurgel <bmg1612@gmail.com>
2021-03-05 13:43:22 -0300

    feat: Added props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 // import './App.css';
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 export default function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = (event) => {
     console.log(event);
   };
@@ -32,13 +32,13 @@ export default function App() {
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
 }
 
-function List() {
-  return list.map(function (item) {
+function List(props) {
+  return props.list.map(function (item) {
     return (
       <div key={item.objectID}>
         <span>
