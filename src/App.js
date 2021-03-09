2028b6c4acb8d71d8f5a02d982d5c1c370d2011d
Bruno Gurgel <bmg1612@gmail.com>
2021-03-09 13:22:07 -0300

    feat: Added try/catch to code

--- src/App.js
+++ src/App.js
@@ -77,17 +77,20 @@ export default function App() {
     isError: false,
   });
 
-  const handleFetchStories = useCallback(() => {
+  const handleFetchStories = useCallback(async () => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) => {
-        dispatchStories({
-          type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+    try {
+      await fetch(url)
+        .then((response) => response.json())
+        .then((result) => {
+          dispatchStories({
+            type: "STORIES_FETCH_SUCCESS",
+            payload: result.hits,
+          });
         });
-      })
-      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+    }
   }, [url]);
 
   useEffect(() => {
